de744ab8b7fdab02b6b71f0de2f66062571f5e7a
MatthewYiHe <matthewhe1@hotmail.com>
2019-05-02 21:16:52 +0000

    forEach added

--- callBackExercise.js
+++ callBackExercise.js
@@ -1,12 +1,23 @@
 // The second argument/parameter is expected to be a function
+function myFunc(element, i){
+  if (element === "Waldo") {
+    found(i);   // execute callback
 
-function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
-    }
   }
 }
+
+function findWaldo(arr, found) {
+  // for (var i = 0; i < arr.length; i++) {
+  arr.forEach(myFunc);
+
+
+     // arr.forEach(function(element){
+     //  console.log(element);
+     //  });
+
+
+}
+
 
 function actionWhenFound(i) {
   console.log("Found Waldo at index", i);
