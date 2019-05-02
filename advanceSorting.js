99cc47fcb7287ecfc076a759d093dd5e644f85d1
MatthewYiHe <matthewhe1@hotmail.com>
2019-05-02 23:40:29 +0000

    name sort

---
+++ advanceSorting.js
@@ -0,0 +1,32 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+console.log(students.sort(compare));
+
+
+
+  function compare(a ,b){
+    if (a.name > b.name){
+      return 1;
+    } else if (a.name < b.name){
+      return -1
+    } else {
+      if (a.age > b.age){
+        return -1;
+      } else if (a.age === b.age){
+        return 0;
+      } return 1
+    }
+  }
+
+
+
+
+
+
+
+
--- test.js
+++ test.js
@@ -1,7 +1,17 @@
-var array1 = [1, 4, 9, 16];
+var array = [5, 2, 1, 3, 10];
 
-// pass a function to map
-const map1 = array1.map(x => x * 2);
 
-console.log(map1);
-// expected output: Array [2, 8, 18, 32]
+
+  // function compare(a ,b){
+  //   if (a > b)
+
+  //   return 1;
+  //   else if (a === b) {
+  //     return 0;
+  //   }
+  //   return -1;
+
+  // }
+
+  console.log(array.sort())
+// console.log(array)
